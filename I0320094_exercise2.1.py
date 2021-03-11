956ce5e2045de48c30cf6913b182640ddc40cb8a
Sekar Zaneta <masayusekar@gmail.com>
2021-03-11 21:07:12 +0700

    exercise

--- I0320094_exercise2.1.py
+++ I0320094_exercise2.1.py
@@ -1,1 +1,18 @@
+#tipe data Boolean
+print(true)
 
+#tipe data String
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Interger
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimel
+print(9a)
+
+#tipe data Complex
+print(5j)
--- I0320094_exercise2.4.py
+++ I0320094_exercise2.4.py
@@ -1,1 +1,13 @@
+#menampilkan informasi program
 
+print("Konversi suhu (Farenheit ke Celcius)")
+
+#input suhu dalam farenheit
+F = float(input("Masukkan suhu(farenheit) : "))
+
+#menampilkan hasil konversi ke layar
+C = 5 * (F-32) / 9
+
+#menampilkan hasil konversi ke layar
+print("Farenheit : ", F)
+print("Celcius : ", C)
