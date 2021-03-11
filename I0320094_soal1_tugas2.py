858c75b34ba8d5a418b0e9968e5329a40f968ff5
Sekar Zaneta <masayusekar@gmail.com>
2021-03-11 22:48:54 +0700

    exercise

--- I0320094_soal1_tugas2.py
+++ I0320094_soal1_tugas2.py
@@ -1,1 +1,71 @@
+### Menghitung Luas Persegi Panjang
 
+# informasi program
+print("===== Menghitung Luas Persegi Panjang =====")
+
+# input panjang dan lebar
+Panjang = float(input("Masukkan panjang: "))
+Lebar = float(input("Masukkan lebar: "))
+
+# proses hitung luas
+LuasPersegiPanjang = Panjang * Lebar
+
+# menampilkan hasil
+print("Luas persegi panjang =", LuasPersegiPanjang, "\n")
+
+### Menghitung Luas Lingkaran
+
+# informasi program
+print("===== Menghitung Luas Lingkaran =====")
+
+# input jari-jari
+r = float(input("Masukkan jari-jari: "))
+
+# proses hitung luas
+
+LuasLingkaran = 3.14 * (r ** 2)
+
+# menampilkan hasil
+print("Luas Lingkaran =", LuasLingkaran, "\n")
+
+### Menghitung Luas Kubus
+
+# informasi program
+print("===== Menghitung Luas Kubus =====")
+
+# input rusuk
+s = float(input("Masukkan panjang rusuk: "))
+
+# proses hitung luas
+LuasKubus = 6 * (s ** 2)
+
+# menampilkan hasil
+print("Luas kubus =", LuasKubus, "\n")
+
+### Menghitung konversi suhu celcius ke fahrenheit
+
+# informasi program
+print("===== Konversi suhu celcius ke fahrenheit =====")
+
+# input celcius
+celcius = float(input("Masukkan suhu celcius: "))
+
+# proses hitung celcius ke fahrenheit
+fahrenheit = (celcius * (9/5))+ 32
+
+# menampilkan hasil
+print(celcius, "celcius =", fahrenheit, "fahrenheit", "\n")
+
+### Menghitung konversi suhu reamur ke kelvin
+
+# informasi program
+print("===== Konversi suhu reamur ke kelvin =====")
+
+# input reamur
+reamur = float(input("Masukkan suhu reamur: "))
+
+# proses hitung celcius ke fahrenheit
+kelvin = (reamur * (5/4)) + 273.15
+
+# menampilkan hasil
+print(reamur, "reamur =", kelvin, "kelvin")
--- I0320094_soal2_tugas2.py
+++ I0320094_soal2_tugas2.py
@@ -1,1 +1,43 @@
+# membuat variabel
+Nama = "Sekar Zaneta Amirulputri"
+Asal = "SMA Islam Al-Azhar 4"
+Status = "Mahasiswa"
+Tempat = "Universitas Sebelas Maret"
+ProgramStudi = "Teknik Industri"
+TahunMasuk = 2020
+TinggiBadan = 160
+BeratBadan = 47.9
+UkuranSepatu = 39
+Alamat = "Taman Galaxy,Bekasi Selatan"
 
+# Tanggal, Bulan dan Tahun Lahir
+TanggalLahir = 9
+BulanLahir = 9           # 9 = September
+TahunLahir = 2002
+
+# Tanggal, Bulan dan Tahun Sekarang
+TanggalSekarang = 11
+BulanSekarang = 3             # 3 = Maret
+TahunSekarang = 2021
+
+# menghitung jumlah bulan
+JumlahBulanLahir = 12 * TahunLahir + BulanLahir + TanggalLahir / 30               # 12 = jumlah bulan tiap tahun
+JumlahBulanSekarang = 12 * TahunSekarang + BulanSekarang + TanggalSekarang / 30   # 30 = jumlah tanggal tiap bulan
+
+# menghitung usia dalam satuan bulan
+UsiaDalamBulan = int(JumlahBulanSekarang - JumlahBulanLahir)
+
+
+#tampilan
+print("===== Biodata =====")
+print("Nama lengkap   :", Nama)
+print("Usia           :", UsiaDalamBulan, "bulan")
+print("Asal SMA       :", Asal)
+print("Pekerjaan      :", Status)
+print("Universitas    :", Tempat)
+print("Program Studi  :", ProgramStudi)
+print("Tahun Masuk    :", TahunMasuk)
+print("Tinggi badan   :", TinggiBadan, "cm")
+print("Berat  badan   :", BeratBadan, "kg")
+print("Tempat tinggal :", Alamat)
+print("Ukuran sepatu  :", UkuranSepatu)
